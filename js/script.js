1a2e5be4911f30eebb02e98bd4f7a3246d7fa58d
mTutM <maks.tutukov@mail.ru>
2021-04-08 20:33:53 +0300

    add js

--- js/script.js
+++ js/script.js
@@ -3,21 +3,22 @@ const form = document.getElementById('form');
 const messages = {
     firstName: {
         empty: 'First Name cannot be empty',
-        invalid: 'First Name can only contain letters'
+        invalid: 'First Name can only contain letters',
     },
     lastName: {
         empty: 'Last Name cannot be empty',
-        invalid: 'Last Name can only contain letters'
+        invalid: 'Last Name can only contain letters',
     },
-    email: {
+    emailAddress: {
         empty: 'Email cannot be empty',
-        invalid: 'Looks like this is not an email'
+        invalid: 'Looks like this is not an email',
     },
     password: {
         empty: 'Password cannot be empty',
-        invalid: 'Password must be at least 8 characters and contain at least one uppercase letter, one lowercase letter and one number'
-    }
-}
+        invalid:
+            'Password must be at least 8 characters and contain at least one uppercase letter, one lowercase letter and one number',
+    },
+};
 
 const validateField = (field, messages) => {
     if (!field.validity.valid) {
@@ -26,18 +27,16 @@ const validateField = (field, messages) => {
         field.setAttribute('aria-invalid', 'true');
 
         updateAlert(field.nextElementSibling, message);
-        
-        return false;
 
+        return false;
     } else {
         field.classList.remove('form_input--invalid');
         field.setAttribute('aria-invalid', 'false');
-
         updateAlert(field.nextElementSibling);
-        
+
         return true;
-    } 
-}
+    }
+};
 
 const updateAlert = (target, message = false) => {
     if (message) {
@@ -47,40 +46,41 @@ const updateAlert = (target, message = false) => {
         target.removeAttribute('role');
         target.innerText = '';
     }
-}
+};
 
 form.addEventListener('submit', (e) => {
+    e.preventDefault();
     let valid = [];
-    
-    form.querySelectorAll('.form_input').forEach( field => {
+    form.querySelectorAll('.form_input').forEach((field) => {
         valid.push(validateField(field, messages));
     });
-    
-    if (!valid.every( isValid => isValid)) {
+
+    if (!valid.every((isValid) => isValid)) {
         e.preventDefault();
         console.log('invalid');
     }
 });
 
-form.addEventListener('focus', (e) => {
-    let field = e.target;
+form.addEventListener(
+    'focus',
+    (e) => {
+        let field = e.target;
+        field.classList.remove('form_input--invalid');
+    },
+    true
+);
 
-    if (field !== form.password) e.target.classList.remove('form_input--invalid');
-
-}, true);
-
-form.addEventListener('blur', (e) => {
-    let field = e.target;
-
-    if (field !== form.password) {
+form.addEventListener(
+    'blur',
+    (e) => {
+        let field = e.target;
         if (field.value !== '') {
             field.value = field.value.trim().replace(/\s+/g, ' ');
             validateField(field, messages);
         }
-    } else {
-        validateField(field, messages);
-    }
-}, true);
+    },
+    true
+);
 
 form.password.addEventListener('input', () => {
     validateField(form.password, messages);
